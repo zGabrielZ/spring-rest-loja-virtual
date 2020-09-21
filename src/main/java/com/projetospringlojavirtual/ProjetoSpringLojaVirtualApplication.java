7190d59f1ae62f47dd99a6893289211a1567c759
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2020-09-21 19:39:54 -0300

    Inicializando o projeto

---
+++ src/main/java/com/projetospringlojavirtual/ProjetoSpringLojaVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.projetospringlojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringLojaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringLojaVirtualApplication.class, args);
+	}
+
+}
