67ae600981a2e43b9543493865cbdbb13cebc6ce
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2020-09-24 13:56:30 -0300

    Liberando CORS

--- src/main/java/com/projeto/spring/lojavirtual/ProjetoSpringLojaVirtualApplication.java
+++ src/main/java/com/projeto/spring/lojavirtual/ProjetoSpringLojaVirtualApplication.java
@@ -2,10 +2,19 @@ package com.projeto.spring.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ProjetoSpringLojaVirtualApplication {
+@EnableWebMvc
+public class ProjetoSpringLojaVirtualApplication implements WebMvcConfigurer{
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET","POST","DELETE","PUT","OPTIONS");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringLojaVirtualApplication.class, args);
 	}
