d5bebfe7946d43a7441b6fca8d91e8b6a89a906f
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2020-10-05 18:55:56 -0300

    Projeto feito

--- src/main/java/com/projeto/spring/lojavirtual/modelo/entidade/dto/ItensInserirDTO.java
+++ src/main/java/com/projeto/spring/lojavirtual/modelo/entidade/dto/ItensInserirDTO.java
@@ -3,6 +3,7 @@ package com.projeto.spring.lojavirtual.modelo.entidade.dto;
 import java.io.Serializable;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
@@ -18,6 +19,7 @@ public class ItensInserirDTO implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 
+	@Min(value = 1,message = "Mínimo é de 1 quantidade")
 	@NotNull(message = "Quantidade não pode ser nulo")
 	private Integer quantidade;
 	
